09f9ff71b0486f4935e39267358659033eb8364a
unknown <matias1.santillan@gmail.com>
2021-04-08 14:51:55 -0300

    searcher completly done

--- src/Components/SearchingMovie.jsx
+++ src/Components/SearchingMovie.jsx
@@ -1,51 +1,16 @@
 import React, { Component } from 'react'
-import Carousel from './Carousel'
 import CarouselItem from './CarouselItem'
 
 import '../assets/styles/components/SearchMovie.css'
 class SearchingMovie extends Component{
-    state={
-        loading:true,
-        error:false,
-        API_KEY:'76ea301b5b0a49273c1693f3ec685b25',
-        data:{
-            results:[{}]
-        }
-    }
-
-
-    componentWillReceiveProps(e){
-        const movie= e.search
-        this.fetchMovie(`https://api.themoviedb.org/3/search/movie?query=${movie}&api_key=${this.state.API_KEY}`)
-    }
-
-    fetchMovie = async url =>{
-
-        const response = await fetch(url)
-        const data = await response.json()
-        console.log(data)
-        if (data.error){
-          this.setState({
-            loading: false,
-            error: true,
-            errorMessage : 'Movie not found'
-        })
-        }else{
-          this.setState({
-            error: false,
-            loading: false,
-            data: data
-        })
-        }
-      }
+    
   
     render(){
         return(
             <>
-                {this.state.loading && <p className="center">Loading...</p>}
-                {this.state.error && <p className="center">Error: {this.state.errorMessage}</p>}
+                
                 <div className="search_page">
-                    {this.state.data.results.map(movie =>(
+                    {this.props.dataMovie.map(movie =>(
                         
                             <CarouselItem 
                             key={movie.id} 
--- src/Container/SearchMoviePage.jsx
+++ src/Container/SearchMoviePage.jsx
@@ -5,15 +5,22 @@ import SearchingMovie from '../Components/SearchingMovie';
 import firebase from 'firebase'
 import 'firebase/auth'
 class SearchMoviePage extends Component{
-  state={
-    search: "",
-    user:''
-  }
   
+  state={
+    loading:true,
+    error:false,
+    API_KEY:'76ea301b5b0a49273c1693f3ec685b25',
+    search: "",
+    user:'',
+    data:{
+        results:[]
+    }
+  }
+
   componentDidMount(){
    let searcher = this.props.history.location.search.substr(1).replace("%20", " ");
     this.setState({ search: searcher })
-
+    this.fetchMovie(`https://api.themoviedb.org/3/search/movie?query=${searcher}&api_key=${this.state.API_KEY}`)
     firebase.auth().onAuthStateChanged(user =>{
       if(user){
         this.setState({ user })
@@ -21,13 +28,40 @@ class SearchMoviePage extends Component{
     })
   }
 
+
+
+
+
+fetchMovie = async url =>{
+
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+    if (data.error){
+      this.setState({
+        loading: false,
+        error: true,
+        errorMessage : 'Movie not found'
+    })
+    }else{
+      this.setState({
+        error: false,
+        loading: false,
+        data: {
+          results: data.results
+        }
+    })
+    }
+}
+
   ToMovies = (id) =>{
     this.props.history.push(`/MovieDetails?${id}`)
   }
   
+  
   searchingMovie = (e) => {
     e.preventDefault()
-    this.props.history.push(`/search?${this.state.search}`)
+    this.fetchMovie(`https://api.themoviedb.org/3/search/movie?query=${this.state.search}&api_key=${this.state.API_KEY}`)
   };
 
 handleSearch = (e) => {
@@ -42,8 +76,8 @@ handleSearch = (e) => {
                 search={this.state.search}
                 onChange={this.handleSearch}/>
           <SearchingMovie onClick={this.ToMovies} 
-          search={this.state.search}
-          userInfo={this.state.user}/>
+          userInfo={this.state.user}
+          dataMovie={this.state.data.results}/>
         </>
       )
   }
